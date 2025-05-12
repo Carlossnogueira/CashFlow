4937f0974636e93e96eabb864744e932c6ad7cde
Carlossnogueira <carlosnogueira.tkdev@gmail.com>
2025-05-12 20:12:21 -0300

    Adicionando validação ao RegisterExpense e atualizando suas diretivas

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,4 +1,5 @@
-﻿using CashFlow.Communication.Requests;
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 
 namespace CashFlow.Application.UseCases.Expenses.Register
@@ -7,8 +8,36 @@ namespace CashFlow.Application.UseCases.Expenses.Register
     {
         public ResponseRegisteredExpenseJson Execute(RequestRegisterExpenseJson request)
         {
-            //TO DO VALIDATIONS
+           
+            Validate(request);
             return new ResponseRegisteredExpenseJson();
+        }
+
+
+        private void Validate(RequestRegisterExpenseJson request)
+        {
+            var tittleIsEmpty = string.IsNullOrWhiteSpace(request.Title);
+            if(tittleIsEmpty)
+            {
+                throw new ArgumentException("The title is required.");
+            }
+
+            if(request.Amout <= 0)
+            {
+                throw new ArgumentException("The amount is required and must be greater than zero.");
+            }
+
+            var result = DateTime.Compare(request.Date, DateTime.UtcNow);
+            if(result > 0)
+            {
+                throw new ArgumentException("The date must be in the past.");
+            }
+
+            var PaymentTypeIsValid = Enum.IsDefined(typeof(PaymentType), request.PaymentType);
+            if(PaymentTypeIsValid == false)
+            {
+                throw new ArgumentException("Payment Type is not valid.");
+            }
         }
     }
 }
