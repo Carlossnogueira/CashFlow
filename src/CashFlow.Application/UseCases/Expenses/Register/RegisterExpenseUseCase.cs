6f6ec295617da014542347397ff615a771fed1e7
Carlossnogueira <carlosnogueira.tkdev@gmail.com>
2025-05-19 19:55:02 -0300

    Adicionando validação ao RegisterExpenseUserCase

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -19,6 +19,13 @@ namespace CashFlow.Application.UseCases.Expenses.Register
 
             var validator = new RegisterExpenseValidator();
             var result = validator.Validate(request);
+
+            if(!result.IsValid)
+            {
+                var errorMessages = result.Errors.Select(f => f.ErrorMessage).ToList();
+
+                throw new ArgumentException(errorMessages.ToString());
+            }
           
         }
     }
